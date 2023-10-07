c8b1368b8062e9430d6647ac4fbd061bb1b967d3
차필우 <bru@chapil-uui-MacBookAir.local>
2023-10-07 13:03:45 +0900

    수정하엿소이다

--- dom/dom/dom.js
+++ dom/dom/dom.js
@@ -3,9 +3,11 @@ window.onload = function () {
     "너무 어렵다 이거 하나 푸는데 2시간 걸리내";
 };
 
-let tab_host = document.querySelector('.tab_host');
-tab_host.onclick = function(){
-    this.style.color = 'red';
-    this.style.backgroundColor = 'pink';
-}
+let tab_host = document.querySelector(".tab_host");
+tab_host.onclick = function () {
+  this.style.color = "red";
+  this.style.backgroundColor = "pink";
+};
 // 3시간 걸렸내요 ...
+
+//3번은 캡쳐링,버블링을 사용해야하나??
--- q2.js
+++ q2.js
@@ -7,13 +7,18 @@ ex) 길이기 80인 배열은 길이가 5로 분해한다면 16개의 배열을 
     ex) [1...80] => [[1...5], [6...10], [11...15], ... , [76...80]]
 */
 
+const arr = Array.from({ length: 80 }, (_, i) => i + 1);
+//1~80[1,2,3,4,5,6]
+
 function division(arr) {
-  let result = []
-  for (let i = 0; i < arr.length; i += 5) result.push(arr.slice(i, i + 5));
-return result
+  let result = [];
+  for (let i = 0; i < arr.length; i += 5) {
+    console.log(arr.slice(i, i + 5)); //이해가 안가내 알아 봐야함
+    // 3. 내가 의도한 데이터 [1~5], [6~10]이 나오는지 확인했어야했고, 왜 그렇게 나오는지 설명할 수 있어야한다
+
+    result.push(arr.slice(i, i + 5));
+  } // 로직은 정답
+  return result; //를 사용하면 왜 console.log(division(arr))를 해야하는지 알아봐야함
 }
-
-console.log(arr)
-
-
-
+console.log(division(arr)); // 2. dvistion의 return으로 작성해놓고 함수 실행을 안했음
+// console.log(arr); -> 1. 선언하지도 않았기 떄문에 콘솔에 찍혔을리가 없다, 에러 발생
