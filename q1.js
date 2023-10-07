fe931d0068ae01a8953c9b5433d3c2518ad92305
차필우 <bru@chapil-uui-MacBookAir.local>
2023-10-07 22:53:53 +0900

    q1내용 수정했습니다

--- q1.js
+++ q1.js
@@ -1,37 +1,24 @@
 /*
-변수 위치 찾기
+변수(변할수있는 모든 문자와숫자) 위치 찾기
 
 변수 a는 main 함수 안에서 사용된다
-function main() {
-  let a;
-  solution();
-}
-
 
 변수 b는 dom api selector로 다른 함수에서 재사용 될 수 있다 
-const b = document.querySelector("");//단일 객체 반환이라서 선택함
-console.log(b)
 
 변수 c와 solution은 각각 main의 매개변수 및 콜백함수이다.
 
-function main(c){}  
-
-function main(solution) {
-  solution();
-  console.log(solution);
-}
-
-
 변수 c는 콜백함수 solution의 매게변수로 사용된다.
 
-변수 d는 main의 콜백함수인 솔루션 함수에서 전달받은 c와 연산할 변수이다.
-변수 e는 main함수의 최종 반환 값으로 solution에서 d와 c를 더한 값의 반환 값이기도 하다.
+변수 d는 <main의 콜백함수인 솔루션 함수에서 전달받은 c>와 연산할 변수이다.
+변수 e는 <main함수의 최종 반환 값으로 solution에서 d와 c를 더한 값의 반환 값>이기도 하다.
 
 문제 해석은 각기 다를 수 있습니다. 중요한 것은 자신만의 관념으로 변수 위치를 찾는 것으로
 따로 답이 존재하지 않습니다.
 */
 
-function solution() {}
+function solution(c) {   //콜백함수  c는 매게변수
+  
+}
 
 function parse() {}
 
@@ -39,7 +26,7 @@ function main() {
   solution();
 }
 
-function main(c, solution) {
+function main(c) {
   let a;
   let d;
   e = c + d;
